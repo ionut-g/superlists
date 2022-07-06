bfa9ac0b49889aca974f2563ed8344d40f423a6e
ionut-g <ionut.gobej@gmail.com>
2022-07-07 02:19:43 +0300

    Functional test no checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
-import unittest
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
+import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -13,7 +16,27 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrive_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn("To-Do", self.browser.title)
-        self.fail('Finish the test')
+        header_text =self.browser.find_element(By.TAG_NAME,'h1').txt
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        inputbox.set_keys('Buy peacock fathers')
+
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        self.fail('Finish the test!')
 
 if __name__ == "__main__":
     unittest.main(warnings='ignore')
