f4c6db49da0c31ae43bb76cb7b49a1faabb3d1dd
ionut-g <ionut.gobej@gmail.com>
2022-07-07 08:42:57 +0300

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -16,27 +16,29 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrive_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn("To-Do", self.browser.title)
-        header_text =self.browser.find_element(By.TAG_NAME,'h1').txt
+        header_text =self.browser.find_element(By.TAG_NAME,'h1').text
         self.assertIn('To-Do', header_text)
 
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
 
-        inputbox.set_keys('Buy peacock fathers')
+        inputbox.send_keys('Buy peacock fathers')
 
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear"
         )
 
         self.fail('Finish the test!')
 
 if __name__ == "__main__":
     unittest.main(warnings='ignore')
+    
