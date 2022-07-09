16787df6d262bed7c3f1669f3dddf7a31d5d9427
ionut-g <ionut.gobej@gmail.com>
2022-07-09 22:20:44 +0300

    Refactoresd functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID,"id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrive_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn("To-Do", self.browser.title)
@@ -31,28 +36,16 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element(By.ID, "id_list_table")
-        print(f"debug:{table.text} {table.tag_name}")
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        print(f"Debug row: {[row.text for row in rows]}")
-
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            f"New to-do item did not appear in table. Contents were:\n{table.text}"
-        )
-
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        
         inputbox = self.browser.find_element(By.ID,'id_new_item')
         inputbox.send_keys('Use peacock feathers to make fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME,'tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make fly',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make fly')
+
         self.fail('Finish the test!')
 
 
