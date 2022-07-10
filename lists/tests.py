fef8b590a3be003290144f05cb52af75ee82a44f
ionut-g <ionut.gobej@gmail.com>
2022-07-10 14:49:33 +0300

    add a message summarising the above, maybe something like new URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -27,21 +27,14 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/',data={"item_text":"A new list item"})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')        
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world')        
 
     def test_only_save_items_when_necesary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_displays_all_list_items(self):
-        Item.objects.create(text="Item1")
-        Item.objects.create(text="Item2")
 
-        response = self.client.get('/')
 
-        self.assertIn('Item1', response.content.decode())
-        self.assertIn('Item1', response.content.decode())
-        
 class ItemModelTest(TestCase):
 
     def test_saving_and_retriving_items(self):
@@ -61,5 +54,10 @@ class ItemModelTest(TestCase):
         self.assertEqual(first_saved_item.text, 'The fist (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
 
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
 
         
